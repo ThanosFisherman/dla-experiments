b5d8a7813fdbcb688c0a49bcf7e0be0ae73a959d
Thanos Psaridis <psaridis@gmail.com>
2024-08-23 15:01:18 +0300

    use different random per platform

--- core/src/main/java/io/github/thanosfisherman/dla/Juniper.kt
+++ core/src/main/java/io/github/thanosfisherman/dla/Juniper.kt
@@ -1,10 +1,17 @@
 package io.github.thanosfisherman.dla
 
+import com.badlogic.gdx.Application
+import com.badlogic.gdx.Gdx
+import com.github.tommyettinger.random.ChopRandom
 import com.github.tommyettinger.random.EnhancedRandom
 import com.github.tommyettinger.random.PouchRandom
 
 object Juniper {
 
-    val random: EnhancedRandom = PouchRandom()
-
+    val random: EnhancedRandom =
+        if (Gdx.app.type == Application.ApplicationType.WebGL) {
+            ChopRandom()
+        } else {
+            PouchRandom()
+        }
 }
