d15d75decf71c05b6baaf72befcbe86481649120
Thanos Psaridis <psaridis@gmail.com>
2024-08-24 15:56:33 +0300

    Refactored keys and introduced screenshot feature

--- core/src/main/java/io/github/thanosfisherman/dla/screen/FirstScreen.kt
+++ core/src/main/java/io/github/thanosfisherman/dla/screen/FirstScreen.kt
@@ -2,7 +2,10 @@ package io.github.thanosfisherman.dla.screen
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.PixmapIO
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.utils.viewport.FitViewport
@@ -17,6 +20,7 @@ import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
 import ktx.graphics.use
+import java.util.zip.Deflater
 
 class FirstScreen : KtxScreen {
     private val gameViewport = FitViewport(800f, 800f)
@@ -35,17 +39,14 @@ class FirstScreen : KtxScreen {
 
             override fun keyUp(keycode: Int): Boolean {
 
-                if (keycode == Keys.SPACE) {
-                    simStarted = !simStarted
-                }
+                when (keycode) {
+                    Keys.SPACE -> simStarted = !simStarted
+                    Keys.H -> simulation.hideWalkers = !simulation.hideWalkers
+                    Keys.D -> {
+                        fps.isRendered = !fps.isRendered; simulation.isDebug = !simulation.isDebug
+                    }
 
-                if (keycode == Keys.D) {
-                    fps.isRendered = !fps.isRendered
-                    simulation.isDebug = !simulation.isDebug
-                }
-
-                if (keycode == Keys.H) {
-                    simulation.hideWalkers = !simulation.hideWalkers
+                    Keys.S -> screenshot()
                 }
                 return true
             }
@@ -55,7 +56,6 @@ class FirstScreen : KtxScreen {
                 return true
             }
         }
-
         simulation = Simulation(
             gameViewport.worldWidth,
             gameViewport.worldHeight,
@@ -77,15 +77,14 @@ class FirstScreen : KtxScreen {
 
         fps.update(delta)
 
-
         if (Gdx.input.isKeyPressed(Keys.ESCAPE))
             Gdx.app.exit()
 
         gameViewport.apply()
-        shape.projectionMatrix = gameViewport.camera.combined
-        shape.begin(ShapeRenderer.ShapeType.Filled)
-        simulation.draw(shape)
-        shape.end()
+
+        shape.use(ShapeRenderer.ShapeType.Filled, gameViewport.camera.combined) {
+            simulation.draw(shape)
+        }
 
 //        vector.set(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f)
 //        gameViewport.camera.unproject(vector)
@@ -106,5 +105,24 @@ class FirstScreen : KtxScreen {
         batch.disposeSafely()
         fps.disposeSafely()
         shape.disposeSafely()
+    }
+
+    private fun screenshot() {
+        val pixmap = Pixmap.createFromFrameBuffer(0, 0, Gdx.graphics.backBufferWidth, Gdx.graphics.backBufferHeight)
+        val pixels = pixmap.getPixels();
+
+        // This loop makes sure the whole screenshot is opaque and looks exactly like what the user is seeing
+        val size = Gdx.graphics.backBufferWidth * Gdx.graphics.backBufferHeight * 4
+        for (i in 3 until size step 4) {
+            pixels.put(i, 255.toByte())
+        }
+
+        PixmapIO.writePNG(
+            FileHandle("mypixmap${(Math.random() * 10000).toInt()}.png"),
+            pixmap,
+            Deflater.DEFAULT_COMPRESSION,
+            true
+        )
+        pixmap.dispose()
     }
 }
