54bcb23230d12f8e374acaab299b26c93f71331f
Thanos Psaridis <psaridis@gmail.com>
2024-08-24 21:54:44 +0300

    new settings

--- core/src/main/java/io/github/thanosfisherman/dla/Config.kt
+++ core/src/main/java/io/github/thanosfisherman/dla/Config.kt
@@ -2,23 +2,12 @@ package io.github.thanosfisherman.dla
 
 object Config {
     const val RADIUS = 1f
-    const val SPEED = 2f
-    const val ITERATIONS = 1500
+    const val SPEED = 3f
+    const val ITERATIONS = 1000
     const val MAX_WALKERS = 1000
     const val MAX_WALKER_LIFESPAN = 100
-    private const val SPLITTER: Int = RADIUS.toInt() * 10
+    private const val SPLITTER: Int = 3
 
     fun toIndex(xy: Float): Int = (xy / SPLITTER).toInt()
 
-// Use these values for the web
-
-    /*
-    const val RADIUS = 1f
-    const val SPEED = 3f
-    const val ITERATIONS = 1000
-    const val MAX_WALKERS = 400
-    const val MAX_WALKER_LIFESPAN = 20
-    private const val SPLITTER: Int = RADIUS.toInt() * 2
-
-    */
 }
